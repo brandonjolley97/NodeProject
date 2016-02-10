c2754041f8768e9fcff79c49045e5a078b4a4a46
brandonjolley97 <brandonjolley97@gmail.com>
2016-02-10 08:24:51 -0700

    Fixed some errors and added some assert statements

--- src/NodeModel/CTECArray.cpp
+++ src/NodeModel/CTECArray.cpp
@@ -7,24 +7,21 @@
 
 #include "CTECArray.h"
 #include <iostream>
+#include <assert.h>
 
 using namespace std;
 
-template <class Type>
+template<class Type>
 CTECArray<Type>::CTECArray(int size)
 {
 	this->size = size;
-	head = nullptr;
+	this->head = nullptr;
 
-	if(size <= 0)
-	{
-		cerr << "Nope. Not Happening!" << endl;
-		return;
-	}
+	assert(size > 0);
 
 	for (int index = 0; index < size; index++)
 	{
-		if(head != nullptr)
+		if (head != nullptr)
 		{
 			//We have more than one ArrayNode
 			ArrayNode<Type> nextNode;
@@ -41,20 +38,20 @@ CTECArray<Type>::CTECArray(int size)
 
 }
 
-template <class Type>
+template<class Type>
 CTECArray<Type>::~CTECArray()
 {
 	ArrayNode<Type> * deleteMe = head;
 	cout << "I'm Still Alive!!!" << endl;
-	for(int index = 0; index < size; index ++)
+	for (int index = 0; index < size; index++)
 	{
-		if(deleteMe->getNext() != nullptr)
+		if (deleteMe->getNext() != nullptr)
 		{
 			head = deleteMe->getNext();
 			deleteMe->setNext(nullptr);
 		}
-			delete deleteMe->getNext();
-			deleteMe = head;
+		delete deleteMe->getNext();
+		deleteMe = head;
 
 	}
 
@@ -62,70 +59,51 @@ CTECArray<Type>::~CTECArray()
 
 }
 
-template <class Type>
-int CTECArray<Type>:: getSize()
+template<class Type>
+int CTECArray<Type>::getSize()
 {
 	return this->size;
 }
 
-template <class Type>
-Type * CTECArray<Type>:: get(int position)
+template<class Type>
+Type CTECArray<Type>::get(int position)
 {
 	//Bounds check for size and negative.
-	if(position >= size || position < 0)
-	{
-		return nullptr;
-		cout << "You done messed up A-A-Ron." << endl;
-	}
-	else
-	{
-		ArrayNode<Type> * current = head;
-		//Inclusive because I am inside the bounds guaranteed
-		for(int spot = 0; spot <= position; spot++)
-		{
-			if(spot != position)
-			{
-				current = current->getNext();
-			}
-			else
-			{
-				Type temp = current->getValue();   //Grab the value stored in the Node
-				return &temp;                      //Return a pointer to the value
-			}
+	assert(position < size && position >= 0);
 
-		}
-	}
-}
-
-template <class Type>
-void CTECArray<Type>:: set(int position, Type value)
-{
-	if(position >= size || position < 0)
+	ArrayNode<Type> * current = head;
+	//Inclusive because I am inside the bounds guaranteed
+	for (int spot = 0; spot <= position; spot++)
+	{
+		if (spot != position)
 		{
-			cerr << "Don't do this!  Out of bounds!" << endl;
+			current = current->getNext();
 		}
 		else
 		{
-			ArrayNode<Type> * current = head;
-			for(int spot = 0; spot <= position; spot++)
-			{
-				if(spot != position)
-				{
-					current = current->getNext();
-				}
-				else
-				{
-					current->setValue(value);
-				}
-			}
+			return current->getValue();
 		}
+
+	}
 }
 
+template<class Type>
+void CTECArray<Type>::set(int position, const Type& value)
+{
+	//Bounds check for size and negative
+	assert(position < size && position >= 0);
 
-
-
-
-
-
-
+	ArrayNode<Type> * current = head;
+	for (int spot = 0; spot <= position; spot++)
+	{
+		if (spot != position)
+		{
+			current = current->getNext();
+		}
+		else
+		{
+			current->setValue(value);
+		}
+	}
+}
 
--- src/NodeModel/CTECArray.h
+++ src/NodeModel/CTECArray.h
@@ -21,8 +21,8 @@ public:
 	CTECArray(int size);
 	virtual ~CTECArray();
 	int getSize();
-	Type * get(int position);
-	void set(int position, Type value);
+	Type get(int position);
+	void set(int position, const Type& value);
 };
 
 #endif /* NODEMODEL_CTECARRAY_H_ */

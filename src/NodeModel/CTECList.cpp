ae44867486425d1efb9c425168b89fb9d6d33cea
brandonjolley97 <brandonjolley97@gmail.com>
2016-02-22 09:17:41 -0700

    created CTECList .h and .cpp classes

---
+++ src/NodeModel/CTECList.cpp
@@ -0,0 +1,20 @@
+/*
+ * CTECList.cpp
+ *
+ *  Created on: Feb 22, 2016
+ *      Author: bjol7457
+ */
+
+#include "CTECList.h"
+
+CTECList::CTECList()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CTECList::~CTECList()
+{
+	// TODO Auto-generated destructor stub
+}
+

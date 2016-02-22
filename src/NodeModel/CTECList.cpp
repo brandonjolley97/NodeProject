6d6849d241a7dc7e767da3d88fa9ce1225999acd
brandonjolley97 <brandonjolley97@gmail.com>
2016-02-22 09:34:31 -0700

    created all class headers in CTECList.cpp

--- src/NodeModel/CTECList.cpp
+++ src/NodeModel/CTECList.cpp
@@ -7,14 +7,94 @@
 
 #include "CTECList.h"
 
-CTECList::CTECList()
+template<class Type>
+CTECList<Type>::CTECList()
 {
-	// TODO Auto-generated constructor stub
+	this->size = 0;
+	this->head = head;
+	this->end = end;
 
 }
 
-CTECList::~CTECList()
+template<class Type>
+CTECList<Type>::~CTECList()
 {
-	// TODO Auto-generated destructor stub
+
 }
+
+template<class Type>
+int CTECList<Type>::getSize()
+{
+
+}
+
+template<class Type>
+void CTECList<Type>::addToFront(const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>::addToEnd(const Type& value)
+{
+
+}
+
+template<class Type>
+void CTECList<Type>::addToIndex(int index, const Type& value)
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::getFront()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::getEnd()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::getFromIndex(int index)
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeFront()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeEnd()
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::removeIndex(int index)
+{
+
+}
+
+template<class Type>
+Type CTECList<Type>::set(int index, const Type& value)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
 
